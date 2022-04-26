74438d00475c20b62d9a2746ec705b8b4b0a15ca
TimCout <timothe.couturier@gmail.com>
2022-04-26 09:13:24 +0200

    Modification du nom des fonctions cpw et cpb

--- TROUVETOUSLESCOUPS.py
+++ TROUVETOUSLESCOUPS.py
@@ -3,51 +3,6 @@ dico={'request': 'play', 'lives': 2, 'errors': [{'message': 'Edwin et Tim2 take 
 lb=dico['state']['board'][0]
 lw=dico['state']['board'][1]
 
-
-
-#ne sert plus
-def coup(liste_black,liste_white):
-    coups_possible=[]
-    for i in liste_black:    #recu(i,-9,lb,lw) recu(i-8) recu(i-7) ...
-        if i-9 in liste_white and i-9*2 not in coups_possible and i-9*2 not in liste_black and i-9*2 not in liste_white:
-            coups_possible.append(i-9*2)
-        if i-9 in liste_white and i-9*3 not in coups_possible and i-9*2 not in liste_black and i-9*2 in liste_white:
-            coups_possible.append(i-9*3) #
-        if i-8 in liste_white and i-8*2 not in coups_possible and i-8*2 not in liste_black and i-8*2 not in liste_white:
-            coups_possible.append(i-8*2)
-        if i-8 in liste_white and i-8*3 not in coups_possible and i-8*2 not in liste_black and i-8*2 in liste_white:
-            coups_possible.append(i-8*3) #
-        if i-7 in liste_white and i-7*2 not in coups_possible and i-7*2 not in liste_black and i-7*2 not in liste_white:
-            coups_possible.append(i-7*2)
-        if i-7 in liste_white and i-7*3 not in coups_possible and i-7*2 not in liste_black and i-7*2 in liste_white:
-            coups_possible.append(i-7*3) #
-        if i-1 in liste_white and i-1*2 not in coups_possible and i-1*2 not in liste_black and i-1*2 not in liste_white:
-            coups_possible.append(i-1*2)
-        if i-1 in liste_white and i-1*3 not in coups_possible and i-1*2 not in liste_black and i-1*2 in liste_white:
-            coups_possible.append(i-1*3) #
-        if i+1 in liste_white and i+1*2 not in coups_possible and i+1*2 not in liste_black and i+1*2 not in liste_white:
-            coups_possible.append(i+1*2)
-        if i+1 in liste_white and i+1*3 not in coups_possible and i+1*2 not in liste_black and i+1*2 in liste_white:
-            coups_possible.append(i+1*3) #
-        if i+7 in liste_white and i+7*2 not in coups_possible and i+7*2 not in liste_black and i+7*2 not in liste_white:
-            coups_possible.append(i+7*2)
-        if i+7 in liste_white and i+7*3 not in coups_possible and i+7*2 not in liste_black and i+7*2 in liste_white:
-            coups_possible.append(i+7*3) #
-        if i+8 in liste_white and i+8*2 not in coups_possible and i+8*2 not in liste_black and i+8*2 not in liste_white:
-            coups_possible.append(i+8*2)
-        if i+8 in liste_white and i+8*3 not in coups_possible and i+8*2 not in liste_black and i+8*2 in liste_white:
-            coups_possible.append(i+8*3) #
-        if i+9 in liste_white and i+9*2 not in coups_possible and i+9*2 not in liste_black and i+9*2 not in liste_white:
-            coups_possible.append(i+9*2)
-        if i+9 in liste_white and i+9*3 not in coups_possible and i+9*2 not in liste_black and i+9*2 in liste_white:
-            coups_possible.append(i+9*3) #
-    return coups_possible
-
-#il faut rendre ça récursif !
-
-
-print(coup(lb,lw))
-
 #j'ai rendu ça récursif
 #ne marche que pour les NWARS 
 #NE PRENDS PAS EN COMPTE LES BORDS !!!!
@@ -60,7 +15,7 @@ def recursifb(lb,lw,cp,i,j,a):
         return(recursifb(lb,lw,cp,i,j,a))
 
 
-def deux(lbl,lwh):
+def cpb(lbl,lwh):
     cpo=[]
     for i in lbl:
         for j in (-9,-8,-7,-1,+1,+7,+8,+9):
@@ -68,7 +23,7 @@ def deux(lbl,lwh):
                 cpo.append(recursifb(lbl,lwh,cpo,i,j,2))
     return cpo
 
-print(deux(lb,lw))
+print(cpb(lb,lw))
 
 def recursifw(lb,lw,cp,i,j,a):
     if i+j in lb and i+j*a not in cp and i+j*a not in lw and i+j*a not in lb:
@@ -77,7 +32,7 @@ def recursifw(lb,lw,cp,i,j,a):
         a=a+1
         return(recursifw(lb,lw,cp,i,j,a))
 
-def trois(lbl,lwh):
+def cpw(lbl,lwh):
     cpo=[]
     for i in lwh:
         for j in (-9,-8,-7,-1,+1,+7,+8,+9):
@@ -85,6 +40,6 @@ def trois(lbl,lwh):
                 cpo.append(recursifw(lbl,lwh,cpo,i,j,2))
     return cpo
 
-print(trois(lb,lw))
+print(cpw(lb,lw))
 
 
