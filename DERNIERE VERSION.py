496751824a4c2a75328959303b9d4503ecfbe8cc
xX-DarkEdwin-Xx <20220@ecam.be>
2022-04-26 10:47:02 +0200

    premier ia dr bg

--- DERNIERE VERSION.py
+++ DERNIERE VERSION.py
@@ -1,8 +1,49 @@
 import socket
 import json
+import random
 
 ipserver="localhost"
 hisserverAddress=(ipserver,3000)    #adresse de l'hôte du serveur
+bord=[0,1,2,3,4,5,6,7,15,23,31,39,47,55,63,62,61,60,59,58,57,56,48,40,32,24,16,8]
+
+def recursifb(lb,lw,cp,i,j,a):
+    if i+j in lb and i+j*a in lw and i+j*a in bord:
+        return None
+
+    if i+j in lw and i+j*a not in cp and i+j*a not in lb and i+j*a not in lw:
+        return i+j*a
+    if i+j in lw and i+j*a not in cp and i+j*a not in lb and i+j*a in lw:
+        a=a+1
+        return(recursifb(lb,lw,cp,i,j,a))
+
+
+def cpob(lbl,lwh):
+    cpo=[]
+    for i in lbl:
+        for j in (-9,-8,-7,-1,+1,+7,+8,+9):
+            if recursifb(lbl,lwh,cpo,i,j,2) != None:
+                cpo.append(recursifb(lbl,lwh,cpo,i,j,2))
+    return cpo
+
+
+def recursifw(lb,lw,cp,i,j,a):
+    if i+j in lb and i+j*a in lb and i+j*a in bord:
+        return None
+    
+    if i+j in lb and i+j*a not in cp and i+j*a not in lw and i+j*a not in lb:
+        return i+j*a
+    if i+j in lb and i+j*a not in cp and i+j*a not in lw and i+j*a in lb:
+        a=a+1
+        return(recursifw(lb,lw,cp,i,j,a))
+    
+
+def cpow(lbl,lwh):
+    cpo=[]
+    for i in lwh:
+        for j in (-9,-8,-7,-1,+1,+7,+8,+9):
+            if recursifw(lbl,lwh,cpo,i,j,2) != None:
+                cpo.append(recursifw(lbl,lwh,cpo,i,j,2))
+    return cpo
 
 def inscription():
 
@@ -12,10 +53,10 @@ def inscription():
         s.connect(hisserverAddress)
         s.send(data.encode())
         response=json.loads(s.recv(2048).decode())   
-        print(response)
-        pong()
+        #print(response)
+        server()
 
-def pong():
+def server():
     with socket.socket() as s:
         myserverAddress=('0.0.0.0',8887)
         s.bind(myserverAddress)
@@ -29,16 +70,31 @@ def pong():
             print(message)
             if message==ping:
                 jeu.send(json.dumps(pong).encode())
-                print(pong)
+                #print(pong)
             else:
                 if message['request']=='play':
-                    liste_black=message['state']['board'][0]
-                    print(liste_black)
-                    liste_white=message['state']['board'][1]
-                    print(liste_white)
+                    lb=message['state']['board'][0]
+                    #print(lb)
+                    lw=message['state']['board'][1]
+                    #print(lw)
+                    
+                    if message['state']['current']==0:
+                        #print("Coups possibles pour les noirs : ")
+                        #print(cpob(lb,lw))
+                        case=random.choice(cpob(lb,lw))
+
+                    if message['state']['current']==1:
+                        #print("Coups possibles pour les blancs : ")
+                        #(cpow(lb,lw))
+                        case=random.choice(cpow(lb,lw))
+
+
+
+
                     filename="move.json"
                     jsonstring='{"response": "move","move": '
-                    case=input("Sur quelle case jouer? ")
+                    
+                    #case=input("Sur quelle case jouer? ")
                     jsonstring+=str(case)+','
                     jsonstring+='"message": "'+str(case)+'"}'  #"Fun message"}'
                     data=json.loads(jsonstring)
@@ -46,8 +102,9 @@ def pong():
                     json.dump(data,file)
                     with open("move.json","r") as file:
                         data=file.read() 
+                    print("vies : ",message['lives'])
                     jeu.send(data.encode())
-                    print(data)
+                    #print(data)
                 
         
 
--- TROUVETOUSLESCOUPS.py
+++ TROUVETOUSLESCOUPS.py
@@ -1,58 +1,16 @@
-dico={'request': 'play', 'lives': 2, 'errors': [{'message': 'Edwin et Tim2 take too long to respond: 17.311607837677002s', 'state': {'players': ['Edwin et Tim2', 'Edwin et Tim1'], 'current': 1, 'board': [[28, 35, 26, 27], [36]]}, 'move': 26}], 'state': {'players': ['Edwin et Tim2', 'Edwin et Tim1'], 'current': 0, 'board': [[35, 26, 27], [29,30,36, 20, 28]]}}
+import random
+dico={'request': 'play', 'lives': 3, 'errors': [], 'state': {'players': ['Edwin et Tim2', 'Edwin et Tim1'], 'current': 0, 'board': [[22, 29, 4, 13, 48, 41, 20, 32, 34, 33], [28, 45, 36, 25, 26, 44, 5, 19, 12, 59, 51, 27, 35, 43]]}}
 #print(dico)
 lb=dico['state']['board'][0]
 lw=dico['state']['board'][1]
+print(dico['lives'])
 
-
-
-#ne sert plus
-def coup(liste_black,liste_white):
-    coups_possible=[]
-    for i in liste_black:    #recu(i,-9,lb,lw) recu(i-8) recu(i-7) ...
-        if i-9 in liste_white and i-9*2 not in coups_possible and i-9*2 not in liste_black and i-9*2 not in liste_white:
-            coups_possible.append(i-9*2)
-        if i-9 in liste_white and i-9*3 not in coups_possible and i-9*2 not in liste_black and i-9*2 in liste_white:
-            coups_possible.append(i-9*3) #
-        if i-8 in liste_white and i-8*2 not in coups_possible and i-8*2 not in liste_black and i-8*2 not in liste_white:
-            coups_possible.append(i-8*2)
-        if i-8 in liste_white and i-8*3 not in coups_possible and i-8*2 not in liste_black and i-8*2 in liste_white:
-            coups_possible.append(i-8*3) #
-        if i-7 in liste_white and i-7*2 not in coups_possible and i-7*2 not in liste_black and i-7*2 not in liste_white:
-            coups_possible.append(i-7*2)
-        if i-7 in liste_white and i-7*3 not in coups_possible and i-7*2 not in liste_black and i-7*2 in liste_white:
-            coups_possible.append(i-7*3) #
-        if i-1 in liste_white and i-1*2 not in coups_possible and i-1*2 not in liste_black and i-1*2 not in liste_white:
-            coups_possible.append(i-1*2)
-        if i-1 in liste_white and i-1*3 not in coups_possible and i-1*2 not in liste_black and i-1*2 in liste_white:
-            coups_possible.append(i-1*3) #
-        if i+1 in liste_white and i+1*2 not in coups_possible and i+1*2 not in liste_black and i+1*2 not in liste_white:
-            coups_possible.append(i+1*2)
-        if i+1 in liste_white and i+1*3 not in coups_possible and i+1*2 not in liste_black and i+1*2 in liste_white:
-            coups_possible.append(i+1*3) #
-        if i+7 in liste_white and i+7*2 not in coups_possible and i+7*2 not in liste_black and i+7*2 not in liste_white:
-            coups_possible.append(i+7*2)
-        if i+7 in liste_white and i+7*3 not in coups_possible and i+7*2 not in liste_black and i+7*2 in liste_white:
-            coups_possible.append(i+7*3) #
-        if i+8 in liste_white and i+8*2 not in coups_possible and i+8*2 not in liste_black and i+8*2 not in liste_white:
-            coups_possible.append(i+8*2)
-        if i+8 in liste_white and i+8*3 not in coups_possible and i+8*2 not in liste_black and i+8*2 in liste_white:
-            coups_possible.append(i+8*3) #
-        if i+9 in liste_white and i+9*2 not in coups_possible and i+9*2 not in liste_black and i+9*2 not in liste_white:
-            coups_possible.append(i+9*2)
-        if i+9 in liste_white and i+9*3 not in coups_possible and i+9*2 not in liste_black and i+9*2 in liste_white:
-            coups_possible.append(i+9*3) #
-    return coups_possible
-
-#il faut rendre ça récursif !
-
-
-print(coup(lb,lw))
-
-#j'ai rendu ça récursif
-#ne marche que pour les NWARS 
-#NE PRENDS PAS EN COMPTE LES BORDS !!!!
+bord=[0,1,2,3,4,5,6,7,15,23,31,39,47,55,63,62,61,60,59,58,57,56,48,40,32,24,16,8]
 
 def recursifb(lb,lw,cp,i,j,a):
+    if i+j in lb and i+j*a in lw and i+j*a in bord:
+        return None
+
     if i+j in lw and i+j*a not in cp and i+j*a not in lb and i+j*a not in lw:
         return i+j*a
     if i+j in lw and i+j*a not in cp and i+j*a not in lb and i+j*a in lw:
@@ -60,7 +18,7 @@ def recursifb(lb,lw,cp,i,j,a):
         return(recursifb(lb,lw,cp,i,j,a))
 
 
-def deux(lbl,lwh):
+def cpob(lbl,lwh):
     cpo=[]
     for i in lbl:
         for j in (-9,-8,-7,-1,+1,+7,+8,+9):
@@ -68,16 +26,19 @@ def deux(lbl,lwh):
                 cpo.append(recursifb(lbl,lwh,cpo,i,j,2))
     return cpo
 
-print(deux(lb,lw))
 
 def recursifw(lb,lw,cp,i,j,a):
+    if i+j in lb and i+j*a in lb and i+j*a in bord:
+        return None
+    
     if i+j in lb and i+j*a not in cp and i+j*a not in lw and i+j*a not in lb:
         return i+j*a
     if i+j in lb and i+j*a not in cp and i+j*a not in lw and i+j*a in lb:
         a=a+1
         return(recursifw(lb,lw,cp,i,j,a))
+    
 
-def trois(lbl,lwh):
+def cpow(lbl,lwh):
     cpo=[]
     for i in lwh:
         for j in (-9,-8,-7,-1,+1,+7,+8,+9):
@@ -85,6 +46,13 @@ def trois(lbl,lwh):
                 cpo.append(recursifw(lbl,lwh,cpo,i,j,2))
     return cpo
 
-print(trois(lb,lw))
+
+print("Coups possibles pour les noirs : ")
+print(cpob(lb,lw))
+
+print("Coups possibles pour les blancs : ")
+print(cpow(lb,lw))
+
+case=random.choice(cpow(lb,lw))
 
 
